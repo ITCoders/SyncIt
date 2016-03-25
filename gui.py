92f23569e968e7fc2bc02de491cf949780194415
Arpit Solanki <solankiarpit1997@gmail.com>
2016-03-25 23:53:24 +0530

    added some gui elements

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'Test.ui'
 #
-# Created: Wed Mar 23 22:15:13 2016
+# Created: Fri Mar 25 13:23:04 2016
 #      by: PyQt4 UI code generator 4.11.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -28,22 +28,43 @@ class Ui_Dialog(object):
         Dialog.setObjectName(_fromUtf8("Dialog"))
         Dialog.resize(400, 300)
         self.pushButton = QtGui.QPushButton(Dialog)
-        self.pushButton.setGeometry(QtCore.QRect(270, 250, 98, 27))
+        self.pushButton.setGeometry(QtCore.QRect(10, 250, 98, 27))
         self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.checkBox = QtGui.QCheckBox(Dialog)
-        self.checkBox.setGeometry(QtCore.QRect(140, 250, 97, 22))
-        self.checkBox.setObjectName(_fromUtf8("checkBox"))
-        self.listWidget = MyListWidget(Dialog)
-        self.listWidget.setGeometry(QtCore.QRect(130, 20, 256, 192))
+        self.listWidget = MyListWidget()
+        self.listWidget.setGeometry(QtCore.QRect(60, 20, 256, 192))
         self.listWidget.setObjectName(_fromUtf8("listWidget"))
+        self.pushButton_2 = QtGui.QPushButton(Dialog)
+        self.pushButton_2.setGeometry(QtCore.QRect(10, 220, 101, 27))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.pushButton_3 = QtGui.QPushButton(Dialog)
+        self.pushButton_3.setGeometry(QtCore.QRect(320, 30, 71, 27))
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        self.pushButton_4 = QtGui.QPushButton(Dialog)
+        self.pushButton_4.setGeometry(QtCore.QRect(320, 70, 71, 27))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_4.setFont(font)
+        self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
+        self.pushButton_5 = QtGui.QPushButton(Dialog)
+        self.pushButton_5.setGeometry(QtCore.QRect(320, 110, 71, 27))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        self.pushButton_5.setFont(font)
+        self.pushButton_5.setObjectName(_fromUtf8("pushButton_5"))
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
-        self.pushButton.setText(_translate("Dialog", "PushButton", None))
-        self.checkBox.setText(_translate("Dialog", "CheckBox", None))
+        self.pushButton.setText(_translate("Dialog", "Choose Dir", None))
+        self.pushButton_2.setText(_translate("Dialog", "Login", None))
+        self.pushButton_3.setText(_translate("Dialog", "GetList", None))
+        self.pushButton_4.setText(_translate("Dialog", "NewFolder", None))
+        self.pushButton_5.setText(_translate("Dialog", "SelectFolder", None))
 class MyListWidget(QtGui.QListWidget) :
     def Clicked(self,item):
         print('clciked item is ',item.text())
+
--- gui_implementation.py
+++ gui_implementation.py
@@ -1,3 +1,8 @@
+import os
+import webbrowser
+#from subprocess import call
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 import sys
 import os
 from PyQt4 import QtGui,QtDesigner,QtCore
@@ -12,12 +17,12 @@ class window(QtGui.QFileDialog):
 		Dialog = QtGui.QDialog()
 		self.ui.setupUi(Dialog)
 		QObject.connect(self.ui.pushButton,SIGNAL("clicked()"),self.onclick_pushbutton)
+		self.login_object = sync()
+		QObject.connect(self.ui.pushButton_2,SIGNAL("clicked()"),self.login_object.Login)
+		QObject.connect(self.ui.pushButton_3,SIGNAL("clicked()"),self.populateListView)
 		Dialog.show()
-		list_widget = self.ui.listWidget
-		list_widget.addItem("item1")
-		list_widget.addItem("item2")
-		list_widget.addItem("item3")
-		list_widget.itemClicked.connect(list_widget.Clicked)
+		self.list_widget = self.ui.listWidget
+		self.list_widget.itemClicked.connect(self.list_widget.Clicked)
 		sys.exit(app.exec_())
 	def onclick_pushbutton(self):
 		# subprocess.call(["nautilus"])
@@ -28,7 +33,43 @@ class window(QtGui.QFileDialog):
 		print(filename)
 	def select_folders(self):
 		dir_name = QFileDialog.getExistingDirectory(self,'open dir','/home/arpit/',QFileDialog.ShowDirsOnly)
-		print(dir_name)
+		self.ROOT_FOLDER_PATH = dir_name		
+		self.ROOT_FOLDER_PATH,self.ROOT_FOLDER_NAME = os.path.split(self.ROOT_FOLDER_PATH)
+		print(self.ROOT_FOLDER_NAME)
+	def populateListView(self,parent='root'):
+		print('hello')
+		self.file_list = self.login_object.ListFolder(parent)
+		print(self.file_list)
+		for element in self.file_list:
+			if type(element) is dict:
+				self.list_widget.addItem(element['title'])
+				print(element['title'])
+			else:
+				print(element)
+				self.list_widget.addItem(element)
+
+class sync :
+	#function for login
+	def Login(self):
+		gauth=GoogleAuth()
+		gauth.LocalWebserverAuth()
+		gauth.Authorize()
+		self.drive=GoogleDrive(gauth)
+		return self.drive
+	def ListFolder(self,parent):
+		filelist=[]
+		file_list = self.drive.ListFile({'q': "'%s' in parents and trashed=false" % parent}).GetList()
+		for f in file_list:
+			if f['mimeType']=='application/vnd.google-apps.folder':	
+				filelist.append({"id":f['id'],"title":f['title'],"list":self.ListFolder(f['id'])})
+			else:
+				filelist.append(f['title'])
+		return filelist
+	# def populateList(self,parent='root'):
+	# 	print('hello')
+	# 	self.file_list = self.ListFolder(parent)
+	# 	print(self.file_list)
+
 if __name__ == '__main__':
 	app = QtGui.QApplication(sys.argv)
 	appcomp = window()
